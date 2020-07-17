88d3a2bd10f2818a8265532ab9bfd07eaac9bcda
Ende Shen <ende@fb.com>
2020-07-16 23:40:06 -0700

    update

---
+++ test2.js
@@ -0,0 +1,10 @@
+window.webkit.messageHandlers.callback.postMessage("Installing paper...");
+
+var canvas = document.getElementById("canvas");
+
+window.webkit.messageHandlers.callback.postMessage("Installing paper...");
+
+canvasWidth = window.innerWidth;
+canvasHeight = window.innerHeight;
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
